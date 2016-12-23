c7c8090e239a31e4401faef8b6ff91e52749c60d
DESKTOP-G0H46T4\Anuroop <anuroop.pnr@gmail.com>
2016-12-23 21:20:11 +0800

    add list view item click listener

--- MyNotes/app/src/main/java/com/anuroop/androidtutorials/mynotes/MainActivity.java
+++ MyNotes/app/src/main/java/com/anuroop/androidtutorials/mynotes/MainActivity.java
@@ -1,13 +1,46 @@
 package com.anuroop.androidtutorials.mynotes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private ListView notesListView;
+    private List<String> notesData = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notesListView = (ListView) findViewById(R.id.notesList);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notesData);
+        notesListView.setAdapter(arrayAdapter);
+
+        notesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                onListItemClick(position);
+            }
+        });
+    }
+
+    /**
+     * This method is called when the user clicks on any notes
+     * @param position the position of the item clicked
+     */
+    private void onListItemClick(int position) {
+        Intent i = new Intent(MainActivity.this, NewNoteActivity.class);
+        i.putExtra("noteId", position);
+        startActivity(i);
     }
 }
